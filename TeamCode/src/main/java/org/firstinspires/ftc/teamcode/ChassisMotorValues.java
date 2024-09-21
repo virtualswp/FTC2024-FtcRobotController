0db3547a1305e2b1e9020ff1b98ca86253d75165
virtualswp <virtualswp@hotmail.com>
2024-09-20 23:06:19 -0500

    Added Comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisMotorValues.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisMotorValues.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+///This class is used to copy data about the chassis motor values
 public class ChassisMotorValues {
+
     public double leftFront;
+
     public double leftRear;
 
     public double rightFront;
