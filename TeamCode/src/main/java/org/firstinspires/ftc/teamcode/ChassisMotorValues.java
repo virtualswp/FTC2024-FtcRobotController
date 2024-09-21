e755345d1545f923e27a0a25d186afb61e65c0c0
virtualswp <virtualswp@hotmail.com>
2024-09-20 23:02:05 -0500

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisMotorValues.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class ChassisMotorValues {
+    public double leftFront;
+    public double leftRear;
+
+    public double rightFront;
+
+    public double rightRear;
+
+
+}
